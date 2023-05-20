0332dd80575d1abbf4a93860e7312810717dbe97
Vinicius-Goi <126573272+Vinicius-Goi@users.noreply.github.com>
2023-05-20 13:19:24 +0000

    Desafios

---
+++ turma-A/aula-0/desafios.java
@@ -0,0 +1,85 @@
+import java.util.Scanner;
+public class desafios {
+    public static void main(String[] args) {
+        
+        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+        Scanner ler = new Scanner(System.in);
+        
+        /*
+        System.out.print("Digite um DDD: ");
+        int ddd = ler.nextInt();
+
+        if (ddd == 11 || ddd == 12 || ddd == 13 || ddd == 14 || ddd == 15 || ddd == 16 || ddd == 17 || ddd == 18 || ddd == 19) {
+            System.out.println("Esse DDD é de São Paulo!");
+        }else if (ddd == 21 || ddd == 22 || ddd == 24){
+            System.out.println("Esse DDD é do Rio de Janeiro!");
+        }else if (ddd == 31 || ddd == 32 || ddd == 33 || ddd == 34 || ddd == 35 || ddd == 37 || ddd == 38){
+            System.out.println("Esse DDD é de Minas Gerais!");
+        }else if (ddd == 27 || ddd == 28){
+            System.out.println("Esse DDD é de Espírito Santo!");
+        }else if (ddd == 61){
+            System.out.println("Esse DDD é de Destrito Federal!");
+        }else if (ddd == 62 || ddd == 64) {
+            System.out.println("Esse DDD é de Goiás!");
+        }else if (ddd == 65 || ddd == 66){
+            System.out.println("Esse DDD é de Mato Grosso!");
+        }else if (ddd == 67){
+            System.out.println("Esse DDD é de Mato Grosso do Sul!");
+        }else if (ddd == 41 || ddd == 42 || ddd == 43 || ddd == 44 || ddd == 45 || ddd == 46){
+            System.out.println("Esse DDD é do Paraná");
+        }else if (ddd == 51 || ddd == 53 || ddd == 54 || ddd == 55){
+            System.out.println("Esse DDD é do Rio Grande do Sul!");
+        }else if (ddd == 47 || ddd == 48 || ddd == 49){
+            System.out.println("Esse DDD é de Santa Catarina!");
+        }else if (ddd == 82){
+            System.out.println("Esse DDD é de Alagoas!");
+        }else if (ddd == 71 || ddd == 73 || ddd == 74 || ddd == 75 || ddd == 77){
+            System.out.println("Esse DDD é de Bahia!");
+        }else if (ddd == 85 || ddd == 88){
+            System.out.println("Esse DDD é de Ceará!");
+        }else if (ddd == 98 || ddd == 99){
+            System.out.println("Esse DDD é de Maranhão!");
+        }else if (ddd == 83){
+            System.out.println("Esse DDD é de Paraíba!");
+        }else if (ddd == 81 || ddd == 87){
+            System.out.println("Esse DDD é de Pernambuco!");
+        }else if (ddd == 86 || ddd == 89){
+            System.out.println("Esse DDD é de Piauí!");
+        }else if (ddd == 84){
+            System.out.println("Esse DDD é de Rio Grande do Norte!");
+        }else if (ddd == 79){
+            System.out.println("Esse DDD é de Sergipe!");
+        }else if (ddd == 68){
+            System.out.println("Esse DDD é do Acre!");
+        }else if (ddd == 96){
+            System.out.println("Esse DDD é do Amapá!");
+        }else if (ddd == 92 || ddd == 97){
+            System.out.println("Esse DDD é do Amazonas!");
+        }else if (ddd == 91 || ddd == 93 || ddd == 94){
+            System.out.println("Esse DDD é do Pará!");
+        }else if (ddd == 69){
+            System.out.println("Esse DDD é de Rondônia!");
+        }else if (ddd == 95){
+            System.out.println("Esse DDD é de Roraima!");
+        }else if (ddd == 63){
+            System.out.println("Esse DDD é de Tocantins!");
+        }else{
+            System.out.println("Digite um valor válido!");
+        }
+        */
+        /*
+        System.out.print("Digite um número: ");
+        int num = ler.nextInt();
+
+        if (num % 2 == 0){
+            System.out.println("Esse número é par!");
+        } else {
+            System.out.println("Esse número é impar!");
+        }
+        */
+
+        
+    }
+
+
+}
