21990be2e2fbdd42d27c0c55e80f599a87fc1bb8
Goi <126573272+Vinicius-Goi@users.noreply.github.com>
2023-08-12 13:24:24 +0000

    aula05

---
+++ turma-A/aula-0/Aula5.java
@@ -0,0 +1,13 @@
+public class Aula5 {
+    public static void main(String[] args) {
+        int [] [] tabela = {{4,6}, {1,7}};
+        
+        for (int i = 0; i < 2; i++){
+            for (int j = 0; j < 2; j++){
+                System.out.println(tabela[i][j]+"");
+            }
+        }
+        System.out.println("");
+        
+    }
+}
