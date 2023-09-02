ddc4c21adf3611627c4c580664fb7d9abd002099
Goi <126573272+Vinicius-Goi@users.noreply.github.com>
2023-09-02 13:40:08 +0000

    aula7

---
+++ turma-A/aula-0/Aula7.java
@@ -0,0 +1,24 @@
+public class Aula7{
+    
+    public static void compeTOP(){
+        System.out.println("compeTOP");
+    }
+
+    public static int somaINT(int n1, int n2){
+        int soma = n1+n2;
+        return soma;
+    }
+
+    public static void tabuada(int n){
+        for(int i=1; i <=10; i++){
+            System.out.println(n + "x" + i + "= " + (n*i));
+        }
+    }
+    public static void main(String[] args) {
+         
+        compeTOP();
+        System.out.println(somaINT(8, 8));
+        tabuada(7);
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+    }
+}
